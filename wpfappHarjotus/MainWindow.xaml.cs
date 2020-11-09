d289aff66c3c7fad52e84d5ab7d902d677a91f12
elias.sivonen@edu.keuda.fi <elias.sivonen@edu.keuda.fi>
2020-11-09 09:37:39 +0200

    Add project files.

---
+++ wpfappHarjotus/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfappHarjotus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                MessageBox.Show("kekw" + rand.Next(1, 100).ToString(), "kekw" + rand.Next(1, 100).ToString(), MessageBoxButton.OK);
+            }
+        }
+    }
+}
